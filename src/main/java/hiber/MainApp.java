003103b1df24f8bf72942b1cf9085d9bdaebb303
Georgy Gribov <sektor162s@mail.ru>
2023-11-27 17:33:54 +0400

    Вроде готово

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,10 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru",new Car("test1",2)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru",new Car("test2",3)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru",new Car("test3",4)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru",new Car("test4",5)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,8 +27,19 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = " + user.getCar().getId());
          System.out.println();
       }
+
+      System.out.println("Получения пользователя по модели и серии машины");
+      String model = "test3";
+      int series = 4;
+      System.out.println("" +
+              "Id = "+ userService.getUserByCar(model,series).getId() + "\n" +
+              "First Name = " + userService.getUserByCar(model,series).getFirstName() + "\n" +
+              "Last Name = " + userService.getUserByCar(model,series).getLastName() + "\n" +
+              "Email = " + userService.getUserByCar(model,series).getEmail() + "\n" +
+              "Car = " + userService.getUserByCar(model,series).getCar().getId());
 
       context.close();
    }
--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -2,9 +2,11 @@ package hiber.model;
 
 import jakarta.persistence.*;
 
+import java.io.Serializable;
+
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements Serializable {
 
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,12 +21,25 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+   @JoinColumn(name = "car_id")
+   Car car;
+
    public User() {}
-   
-   public User(String firstName, String lastName, String email) {
+
+   public Car getCar() {
+      return car;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
+   }
+
+   public User(String firstName, String lastName, String email, Car car) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+      this.car = car;
    }
 
    public Long getId() {
